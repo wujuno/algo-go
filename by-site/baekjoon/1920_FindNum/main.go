f7729da2ea16e96934fa81b867de3f0246109736
jhlee <jhlee@antline.com>
2025-09-03 10:31:58 +0900

    feat(boj): 1920 수 찾기 풀이

---
+++ by-site/baekjoon/1920_FindNum/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N,M int
+	fmt.Fscan(in, &N)
+	
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(in, &A[i])
+	}
+	sort.Ints(A)
+
+	
+	fmt.Fscan(in, &M)
+	for i := 0; i < M; i++ {
+		var x int
+		fmt.Fscan(in, &x)
+
+		idx := sort.SearchInts(A, x)
+		if idx < len(A) && A[idx] == x {
+			fmt.Fprintln(out, 1)
+		} else {
+			fmt.Fprintln(out, 0)
+		}
+	}
+}
