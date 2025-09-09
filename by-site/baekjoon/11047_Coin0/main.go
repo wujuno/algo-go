8ff317c4133d158153d27cd11091ee9677078dd2
wujuno <huj88854@gmail.com>
2025-09-09 10:57:39 +0900

    feat(boj): 11047 동전 0 풀이

---
+++ by-site/baekjoon/11047_Coin0/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N, K int
+	fmt.Fscan(in, &N, &K)
+	
+	coins := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(in, &coins[i])
+	}
+
+	count := 0
+
+	for i := N - 1; i >= 0; i-- {
+		if coins[i] > K {
+			continue
+		}
+		use := K / coins[i]
+		count += use
+		K -= use * coins[i]
+		if K == 0 {
+			break
+		}
+	}
+
+	fmt.Fprintln(out, count)
+}
