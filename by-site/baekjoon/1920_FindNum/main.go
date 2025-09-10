b9ea293e4fd5d32bc0dc60038edef39f0d4ef84e
wujuno <huj88854@gmail.com>
2025-09-10 15:03:43 +0900

    feat(boj): 1920 재풀이

--- by-site/baekjoon/1920_FindNum/main.go
+++ by-site/baekjoon/1920_FindNum/main.go
@@ -8,6 +8,37 @@ import (
 )
 
 func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N, M int
+	fmt.Fscan(in, &N)
+	arr := make([]int,0, N)
+	for i := 0; i < N; i++ {
+		var x int
+		fmt.Fscan(in, &x)
+		arr = append(arr, x)
+	}
+
+	sort.Ints(arr)
+
+	fmt.Fscan(in, &M)
+	for i := 0 ; i < M ; i++ {
+		var num int
+		fmt.Fscan(in,&num)
+
+		idx := sort.SearchInts(arr, num)
+		if idx < len(arr) && arr[idx] == num {
+			fmt.Fprintln(out, 1)
+		} else {
+			fmt.Fprintln(out, 0)
+		}
+	}
+	
+}
+
+/* func main() {
 
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -35,4 +66,4 @@ func main() {
 			fmt.Fprintln(out, 0)
 		}
 	}
-}
+} */
