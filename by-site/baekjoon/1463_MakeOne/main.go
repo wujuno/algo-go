5c4017a5e2e8cb3fc5c799f3be8ec621a2fb9249
wujuno <huj88854@gmail.com>
2025-09-15 09:06:25 +0900

    feat(boj): 1463 재풀이

--- by-site/baekjoon/1463_MakeOne/main.go
+++ by-site/baekjoon/1463_MakeOne/main.go
@@ -14,6 +14,32 @@ func main() {
 	var X int
 	fmt.Fscan(in, &X)
 
+
+
+	dp := make([]int, X+1)
+
+	for i := 2; i <= X; i++ {
+		best := dp[i-1] + 1
+		if i%2 == 0 && dp[i/2]+1 < best {
+			best = dp[i/2] + 1
+		}
+		if i%3 == 0 && dp[i/3]+1 < best {
+			best = dp[i/3] + 1
+		}
+		dp[i] = best
+	}
+
+	fmt.Fprintln(out, dp[X])
+}
+
+/* func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var X int
+	fmt.Fscan(in, &X)
+
 	if X <= 1 {
 		fmt.Fprintln(out, 0)
 		return
@@ -35,7 +61,7 @@ func main() {
 	}
 
 	fmt.Fprintln(out, dp[X])
-}
+} */
 
 
 //큰 문제의 최적해(최소 연산 수)가 더 작은 문제들의 최적해에서 만들어진다는 성질을 이용해, 작은 수부터 차근차근 “최솟값”을 저장해 가는 방식이에요.
