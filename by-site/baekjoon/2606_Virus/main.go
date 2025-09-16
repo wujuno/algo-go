078c37e71e12d97434ebdd07048c2d9c4551dd47
wujuno <huj88854@gmail.com>
2025-09-16 15:57:22 +0900

    feat(boj): 2606 재풀이

--- by-site/baekjoon/2606_Virus/main.go
+++ by-site/baekjoon/2606_Virus/main.go
@@ -25,6 +25,47 @@ func main() {
 	}
 
 	vis := make([]bool, N+1)
+	q:= make([]int, N)
+	head:=0
+	q[0] = 1
+	count := 0
+	vis[1] = true
+
+	for head < len(q) {
+		cur:= q[head]
+		head++
+		for _, v := range g[cur] {
+			if !vis[v] {
+				vis[v] = true
+				q = append(q, v)
+				count++
+			}
+
+		}
+
+	}
+	fmt.Fprintln(out, count)
+}
+
+/* func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N, M int
+	fmt.Fscan(in, &N, &M)
+
+	g := make([][]int, N+1)
+
+	for i :=0; i < M; i++ {
+		var a,b int
+		fmt.Fscan(in, &a, &b)
+
+		g[a] = append(g[a], b)
+		g[b] = append(g[b], a)
+	}
+
+	vis := make([]bool, N+1)
 	vis[1]= true
 	q := make([]int, N)
 	head := 0
@@ -47,3 +88,4 @@ func main() {
 
 	fmt.Fprintln(out, count)
 }
+ */
