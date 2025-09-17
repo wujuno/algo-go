729dc2ff106e24bfe83af009289e03c86d6175f3
wujuno <huj88854@gmail.com>
2025-09-17 15:31:07 +0900

    feat(boj): 1927 재풀이

--- by-site/baekjoon/1927_MinHeap/main.go
+++ by-site/baekjoon/1927_MinHeap/main.go
@@ -7,17 +7,16 @@ import (
 	"os"
 )
 
-type IntMinHeap []int
+type IntMinHead []int
 
-func (h IntMinHeap) Len() int            { return len(h) }
-func (h IntMinHeap) Less(i, j int) bool  { return h[i] < h[j] } // 최소 힙
-func (h IntMinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *IntMinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
-func (h *IntMinHeap) Pop() interface{} {
+func (h IntMinHead) Len() int {return len(h)}
+func (h IntMinHead) Less(i, j int) bool {return h[i] < h[j]}
+func (h IntMinHead) Swap(i, j int) {h[i], h[j] = h[j], h[i]}
+func (h *IntMinHead) Push(x interface{}) {*h = append(*h, x.(int))}
+func (h *IntMinHead) Pop() interface{} {
 	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[:n-1]
+	x := old[len(old)-1]
+	*h = old[:len(old)-1]
 	return x
 }
 
@@ -29,22 +28,22 @@ func main() {
 	var N int
 	fmt.Fscan(in, &N)
 
-	h := &IntMinHeap{}
+	h := &IntMinHead{}
 	heap.Init(h)
 
-	for i := 0; i < N; i++ {
-		var x int
-		fmt.Fscan(in, &x)
+	for i:=0 ; i <N; i++ {
+		var num int 
+		fmt.Fscan(in, &num)
 
-		if x == 0 {
+		if num != 0 {
+			heap.Push(h ,num)
+		} else {
 			if h.Len() == 0 {
 				fmt.Fprintln(out, 0)
 			} else {
-				minVal := heap.Pop(h).(int)
-				fmt.Fprintln(out, minVal)
+				minValue := heap.Pop(h)
+				fmt.Fprintln(out, minValue)
 			}
-		} else {
-			heap.Push(h, x)
 		}
 	}
 }
