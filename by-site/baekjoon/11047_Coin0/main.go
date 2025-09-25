1eb9718c2fc8a9bfa61096e74b6f8c4a6a924235
wujuno <huj88854@gmail.com>
2025-09-25 13:13:57 +0900

    feat(boj): 11047 재풀이

--- by-site/baekjoon/11047_Coin0/main.go
+++ by-site/baekjoon/11047_Coin0/main.go
@@ -21,6 +21,38 @@ func main() {
 
 	count := 0
 
+	for i := N-1; i >= 0; i-- {
+		if coins[i] > K {
+			continue
+		}
+
+		use := K / coins[i]
+		count += use
+		K -= use * coins[i]
+		if K == 0 {
+			break
+		}
+
+	}
+
+	fmt.Fprintln(out, count)
+}
+
+/* func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N, K int
+	fmt.Fscan(in, &N, &K)
+	
+	coins := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(in, &coins[i])
+	}
+
+	count := 0
+
 	for i := N - 1; i >= 0; i-- {
 		if coins[i] > K {
 			continue
@@ -34,4 +66,4 @@ func main() {
 	}
 
 	fmt.Fprintln(out, count)
-}
+} */
