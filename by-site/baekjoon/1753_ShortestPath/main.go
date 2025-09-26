15a1b0a87b3dd8b61c39b9ea4eaa3ff4540f3ce7
wujuno <huj88854@gmail.com>
2025-09-26 10:22:56 +0900

    feat(boj): 1753 재풀이

--- by-site/baekjoon/1753_ShortestPath/main.go
+++ by-site/baekjoon/1753_ShortestPath/main.go
@@ -16,9 +16,89 @@ type Item struct {
 	node int
 	dist int
 }
+
 type MinHeap []Item
 
-func (h MinHeap) Len() int            { return len(h) }
+func (h MinHeap) Len() int {return len(h)}
+func (h MinHeap) Less(i, j int) bool {return h[i].dist < h[j].dist}
+func (h MinHeap) Swap(i, j int) {h[i], h[j] = h[j], h[i]}
+func (h *MinHeap) Push(x interface{}) {*h = append(*h, x.(Item))}
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	lastIdx := len(old)-1
+	x := old[lastIdx]
+	*h = old[:lastIdx]
+	return x
+}
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var V, E int
+	fmt.Fscan(in, &V, &E)
+	var K int
+	fmt.Fscan(in, &K)
+
+	g := make([][]Edge, V+1)
+	for i :=0; i < E; i++ {
+		var u, v, w int
+		fmt.Fscan(in, &u, &v, &w)
+		g[u] = append(g[u], Edge{to: v, w:w})
+	} 
+
+	const INF = int(1 << 60)
+	dist := make([]int, V+1)
+	for i := 1; i <=V; i++ {
+		dist[i] = INF
+	}
+	dist[K] = 0
+
+	h := &MinHeap{}
+	heap.Init(h)
+	heap.Push(h, Item{node: K, dist: 0})
+
+	for h.Len() > 0 {
+		cur := heap.Pop(h).(Item)
+		if cur.dist != dist[cur.node] {
+			continue
+		}
+
+		for _, e := range g[cur.node] {
+			nd := cur.dist + e.w
+			if nd < dist[e.to] {
+				dist[e.to] = nd
+				heap.Push(h, Item{node : e.to, dist: nd})
+			}
+		}
+	}
+	for i := 1; i <= V; i++ {
+		if dist[i] == INF {
+			fmt.Fprintln(out, "INF")
+		} else {
+			fmt.Fprintln(out, dist[i])
+		}
+	}
+}
+
+
+
+
+
+
+/* type Edge struct {
+	to int
+	w  int
+}
+ */
+/* type Item struct {
+	node int
+	dist int
+} */
+/* type MinHeap []Item */
+
+/* func (h MinHeap) Len() int            { return len(h) }
 func (h MinHeap) Less(i, j int) bool  { return h[i].dist < h[j].dist }
 func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
 func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(Item)) }
@@ -28,9 +108,9 @@ func (h *MinHeap) Pop() interface{} {
 	it := old[n-1]
 	*h = old[:n-1]
 	return it
-}
+} */
 
-func main() {
+/* func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
@@ -79,4 +159,4 @@ func main() {
 			fmt.Fprintln(out, dist[i])
 		}
 	}
-}
+} */
