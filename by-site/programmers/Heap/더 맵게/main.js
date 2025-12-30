30b8d4d788666f075d1b41b1d4520e9003f521e0
wujuno <huj88854@gmail.com>
2025-12-30 13:55:34 +0900

    Feat: PRO heap / 더 맵게 재풀이

--- by-site/programmers/Heap/더 맵게/main.js
+++ by-site/programmers/Heap/더 맵게/main.js
@@ -1,108 +1,4 @@
-/* class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  push(val) {
-    this.heap.push(val);
-    this.bubbleUp();
-  }
-
-  pop() {
-    if (this.size() === 0) return null;
-    if (this.size() === 1) return this.heap.pop();
-
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown();
-    return min;
-  }
-
-  peek() {
-    return this.heap[0];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[parentIndex] <= this.heap[index]) break;
-      [this.heap[parentIndex], this.heap[index]] = [
-        this.heap[index],
-        this.heap[parentIndex],
-      ];
-      index = parentIndex;
-    }
-  }
-
-  bubbleDown() {
-    let index = 0;
-    const length = this.heap.length;
-    while (true) {
-      let leftChildIndex = 2 * index + 1;
-      let rightChildIndex = 2 * index + 2;
-      let swapIndex = null;
-
-      if (leftChildIndex < length) {
-        if (this.heap[leftChildIndex] < this.heap[index]) {
-          swapIndex = leftChildIndex;
-        }
-      }
-
-      if (rightChildIndex < length) {
-        if (
-          (swapIndex === null &&
-            this.heap[rightChildIndex] < this.heap[index]) ||
-          (swapIndex !== null &&
-            this.heap[rightChildIndex] < this.heap[leftChildIndex])
-        ) {
-          swapIndex = rightChildIndex;
-        }
-      }
-
-      if (swapIndex === null) break;
-      [this.heap[index], this.heap[swapIndex]] = [
-        this.heap[swapIndex],
-        this.heap[index],
-      ];
-      index = swapIndex;
-    }
-  }
-}
-
-function solution(scoville, K) {
-  let answer = 0;
-  const heap = new MinHeap();
-
-  // 1. 모든 요소를 힙에 삽입
-  for (const s of scoville) {
-    heap.push(s);
-  }
-
-  // 2. 가장 작은 값이 K보다 작을 동안 반복
-  while (heap.peek() < K) {
-    // 음식이 하나 남았는데도 K 미만이면 불가능한 경우
-    if (heap.size() < 2) {
-      return -1;
-    }
-
-    const first = heap.pop();
-    const second = heap.pop();
-    const mixed = first + second * 2;
-
-    heap.push(mixed);
-    answer++;
-  }
-
-  return answer;
-}
- */
-
-function solution(scoville, K) {
+/* function solution(scoville, K) {
   let answer = 0;
   const heap = [];
 
@@ -158,5 +54,63 @@ function solution(scoville, K) {
     answer++;
   }
 
+  return answer;
+} */
+
+function solution(scoville, K) {
+  var answer = 0;
+  const heap = []; // 일단 heap을 생성
+
+  function push(val) {
+    heap.push(val); // 일단 오른쪽 끝에 추가
+    let cur = heap.length - 1; // 추가된 값의 현재 인덱스
+
+    while (cur > 0) {
+      let parent = Math.floor((cur - 1) / 2); // 식은 인데스 트리로 그려보면 나옴
+      if (heap[parent] <= heap[cur]) break; // 꼭대기가 제일 작은 값이므로 부모가 더 작으면 stop
+      else {
+        [heap[cur], heap[parent]] = [heap[parent], heap[cur]]; // 부모값과 자식 값 교환
+        cur = parent; // index 업데이트
+      }
+    }
+  }
+
+  function pop() {
+    if(heap.length == 0) return null; // heap의 길이가 0과 1일때 우선 리턴
+    if(heap.length == 1) return heap.pop()
+
+    const min = heap[0] // 리턴할 최소값 먼저 대입
+    heap[0] = heap.pop() // 첫번째 인덱스에 마지막 값 넣기
+
+    let cur = 0 
+
+    while(cur *2 + 1 < heap.length) {
+      let left = cur * 2 + 1
+    let right = cur * 2 + 2
+
+    let smaller = right < heap.length && heap[right] < heap[left] ? right : left 
+
+    if (heap[cur] <= heap[smaller]) break
+    else {
+      [heap[cur], heap[smaller]] = [heap[smaller], heap[cur]];
+      cur = smaller
+    }
+    }
+    
+    return min
+  }
+
+  for (let s of scoville) push(s)
+  
+  while(heap[0] < K) {
+    if(heap.length < 2) return -1
+
+     const first = pop();
+    const second = pop();
+    const mixed = first + second * 2;
+
+    push(mixed);
+    answer++;
+  }
   return answer;
 }
