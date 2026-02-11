a9926a119a84857ba90926a908c9fb5a2a9f96b8
wujuno <huj88854@gmail.com>
2026-02-11 17:35:48 +0900

    Feat: PRO 크레인 인형뽑기 게임 재풀이

--- by-site/programmers/stack,queue/크레인_인형뽑기_게임.js
+++ by-site/programmers/stack,queue/크레인_인형뽑기_게임.js
@@ -1,29 +1,24 @@
 function solution(board, moves) {
-    const stack = [];
-    let count = 0;
-
-    for (const v of moves) {
-        const area = v - 1;
+    const queue = []
+    let count = 0
+    moves.forEach(v => {
+        const idx = v - 1
 
         for (let i = 0; i < board.length; i++) {
-            if (board[i][area] !== 0) {
-                const doll = board[i][area]; // 현재 집은 인형
-                board[i][area] = 0; // 인형을 집었으므로 빈 공간으로 만듦
-
-                // 바구니에 인형이 있고, 맨 위 인형과 현재 인형이 같다면
-                if (stack.length > 0 && stack[stack.length - 1] === doll) {
-                    stack.pop();
-                    count += 2;
+            if (board[i][idx] == 0) continue
+            else {
+                if (queue.length != 0 && queue[queue.length - 1] == board[i][idx]) {
+                    queue.pop()
+                    count += 2
                 } else {
-                    // 일치하지 않을 때만 바구니에 넣음
-                    stack.push(doll);
+                    queue.push(board[i][idx])
                 }
 
-                // 중요: 인형을 하나 집었으므로 이 move(열 탐색)는 종료
-                break;
+                board[i][idx] = 0
+                break
             }
         }
-    }
+    })
 
-    return count;
+    return count
 }
